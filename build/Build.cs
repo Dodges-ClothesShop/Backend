9f2e3ac85502dd4b57c2c83759283cbb81ee7d67
Deathstro-k <sanechichek.trofimov@gmail.com>
2024-02-20 20:33:14 +0300

    #3 Установлен nuke.build

---
+++ build/Build.cs
@@ -0,0 +1,18 @@
+using Nuke.Common;
+
+class Build : NukeBuild
+{
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+        });
+
+}
